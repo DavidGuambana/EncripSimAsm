a48ce9ed6d807dee9cdfce55f72ee12e3e7e76bf
David Guambaña <davichofan@hotmail.com>
2025-05-08 17:09:16 -0500

    finalizado

---
+++ src/main/java/com/david/encripsimasm/EncripSimAsmApplication.java
@@ -0,0 +1,13 @@
+package com.david.encripsimasm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncripSimAsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncripSimAsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/encripsimasm/EncripSimAsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.encripsimasm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncripSimAsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
